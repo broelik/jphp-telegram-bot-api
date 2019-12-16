f368bc3f9b2e015e422a6a9988c063cc5a4ac050
Ts.Saltan <TsSaltan@gmail.com>
2019-12-16 19:51:58 +0300

    Update TInlineKeyboard.php

--- src/telegram/object/markup/TInlineKeyboard.php
+++ src/telegram/object/markup/TInlineKeyboard.php
@@ -1,6 +1,8 @@
 <?php
 namespace telegram\object\markup;
 
+use telegram\exception\TelegramException;
+
 /**
  * --EN--
  * This object represents an inline keyboard that appears right next to the message it belongs to.
@@ -12,17 +14,36 @@ namespace telegram\object\markup;
  */
 class TInlineKeyboard extends TReplyKeyboard {
 	/**
+	 * Callback data limit
+	 */
+	const MAX_CBDATA_LENGTH = 64;
+
+	/**
 	 * Добавить кнопку
 	 * @param  string   $text	Текст кнопки
 	 * @param  string 	$data 	Данные, которые будут возвращены при нажатии или ссылка для перехода
 	 * @return TInlineKeyboard
 	 */
-	public function button(string $text, string $data){
+	public function button(string $text){
 		$btn = ['text' => $text];
 
-		if(strpos($data, 'http:') === 0 || strpos($data, 'https:') === 0){
+		if(func_num_args() < 2){
+			throw new TelegramException('Parameter callback_data required for InlineKeyboard buttons');
+		} else {
+			$data = func_get_arg(1);
+		}
+
+		if(!is_string($data)){
+			throw new TelegramException('Parameter callback_data should be a string');
+		}
+
+		if(strpos($data, 'http:') === 0 || strpos($data, 'https:') === 0 || strpos($data, 'tg:') === 0){
 			$btn['url'] = $data;
 		} else {
+			if(strlen($data) > self::MAX_CBDATA_LENGTH){
+				throw new TelegramException('Parameter callback_data should be a string no more than ' . MAX_CBDATA_LENGTH . ' bytes in size');
+			}
+			
 			$btn['callback_data'] = $data;
 		}
 
